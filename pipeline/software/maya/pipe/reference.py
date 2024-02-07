d826b2c5ebdb93d090f73127b44738176223bfa0
Student User <thechrisluangrath@gmail.com>
2024-02-07 14:32:56 -0700

    fixing import rig again

--- pipeline/software/maya/pipe/reference.py
+++ pipeline/software/maya/pipe/reference.py
@@ -23,45 +23,44 @@ def logMessage(logName, message):
 rig_path_win = 'G:\\shrineflow\\assets\\Characters\\Rigs\\'
 rig_path_lin = '/groups/shrineflow/assets/Characters/Rigs/'
 
-class reference:
 
-    def ref(self, filePath, namespace):
-        mc.file(filePath, r = True, namespace = namespace)
+def ref(self, filePath, namespace):
+    mc.file(filePath, r = True, namespace = namespace)
 
-    def rig(self, rig_name):
-        if platform.system() == 'Windows':
-            filePath = f'{rig_path_win}{rig_name}.mb'
-        elif platform.system() == 'Linux':
-            filePath = f'{rig_path_lin}{rig_name}.mb'
+def rig(self, rig_name):
+    if platform.system() == 'Windows':
+        filePath = f'{rig_path_win}{rig_name}.mb'
+    elif platform.system() == 'Linux':
+        filePath = f'{rig_path_lin}{rig_name}.mb'
 
-        # if platform.system() == 'Windows':
-        #     # if rig_name == ninja_rig_name:
-        #         filePath = f'G:\\shrineflow\\assets\\Characters\\Rigs\\{rig_name}.mb'
+    # if platform.system() == 'Windows':
+    #     # if rig_name == ninja_rig_name:
+    #         filePath = f'G:\\shrineflow\\assets\\Characters\\Rigs\\{rig_name}.mb'
 
-        #     # elif rig_name == 'kitsune':
-        #         # filePath = 'G:\\shrineflow\\assets\\Characters\\Rigs\\Kitsune_Rig.mb'
-                
-        # elif platform.system() == 'Linux':
-        #     if rig_name == 'ninja':
-        #         filePath = '/groups/shrineflow/assets/Characters/Rigs/Ninja_Rig.mb'
+    #     # elif rig_name == 'kitsune':
+    #         # filePath = 'G:\\shrineflow\\assets\\Characters\\Rigs\\Kitsune_Rig.mb'
+            
+    # elif platform.system() == 'Linux':
+    #     if rig_name == 'ninja':
+    #         filePath = '/groups/shrineflow/assets/Characters/Rigs/Ninja_Rig.mb'
 
-        #     elif rig_name == 'kitsune':
-        #         filePath = '/groups/shrineflow/assets/Characters/Rigs/Kitsune_Rig.mb'
+    #     elif rig_name == 'kitsune':
+    #         filePath = '/groups/shrineflow/assets/Characters/Rigs/Kitsune_Rig.mb'
 
-        self.ref(filePath, rig_name)
-        # self.ref(filePath, rig_name)
+    self.ref(filePath, rig_name)
+    # self.ref(filePath, rig_name)
 
-    '''def camera(self):
-        env = environment.Environment()
-        filePath = mc.file(q=True, sn=True)
-        curDir = env.get_file_dir(filePath)[1:]
-        if (filePath.find('/groups/unfamiliar/anim_pipeline/production/anim_shots') == -1):
-            logMessage('Camera Importer', 'Not in a valid shot file. Please check out a shot and try again.')
-            return
-        shotName = curDir.split('/')[-1]
-        shotsDir = '/groups/unfamiliar/anim_pipeline/production/shots'
-        camPath = shotsDir + '/' + shotName + '/camera/camera_main.fbx'
-        if os.path.exists(camPath) == False:
-            logMessage('Camera Importer', 'No camera has been published for this shot.')
-            return
-        self.ref(camPath, shotName)'''
+'''def camera(self):
+    env = environment.Environment()
+    filePath = mc.file(q=True, sn=True)
+    curDir = env.get_file_dir(filePath)[1:]
+    if (filePath.find('/groups/unfamiliar/anim_pipeline/production/anim_shots') == -1):
+        logMessage('Camera Importer', 'Not in a valid shot file. Please check out a shot and try again.')
+        return
+    shotName = curDir.split('/')[-1]
+    shotsDir = '/groups/unfamiliar/anim_pipeline/production/shots'
+    camPath = shotsDir + '/' + shotName + '/camera/camera_main.fbx'
+    if os.path.exists(camPath) == False:
+        logMessage('Camera Importer', 'No camera has been published for this shot.')
+        return
+    self.ref(camPath, shotName)'''
