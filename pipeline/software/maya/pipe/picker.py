c177a0f82608fe53e46dbab8bc5a62363b140d0f
Christian Chantabury Luangrath <thechrisluangrath@gmail.com>
2024-02-15 17:06:43 -0700

    picker swaps json files depending on os, sorry I keep working in prod

--- pipeline/software/maya/pipe/picker.py
+++ pipeline/software/maya/pipe/picker.py
@@ -3,9 +3,14 @@ from . import utils
 
 path_to_picker = ["working_files", "Animation", "Picker_files"]
 
-picker_filenames = [
+picker_filenames_windows = [
     "Ninja_Picker.json",
     "Kitsune_Picker.json"
+]
+
+picker_filenames_linux = [
+    "Ninja_Picker_linux.json",
+    "Kitsune_Picker_linux.json"
 ]
 
 def run():
@@ -18,7 +23,7 @@ def run():
     import dwpicker
 
     picker_folder_path = os.path.join(utils.get_path_to_groups_folder(), *path_to_picker)
-    picker_filepaths = [os.path.join(picker_folder_path, picker_filename) for picker_filename in picker_filenames]
+    picker_filepaths = [os.path.join(picker_folder_path, picker_filename) for picker_filename in (picker_filenames_windows if os.name == "nt" else picker_filenames_linux)]
     print("Picker filepaths", picker_filepaths)
 
     dwpicker.show(pickers=picker_filepaths)
